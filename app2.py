7ea6fcbe0b3f9918a3936dcb0db5f65e37c18174
daiki2828 <barce.flump0321@gmail.com>
2023-02-08 18:32:46 +0900

    修正

--- app2.py
+++ app2.py
@@ -65,32 +65,30 @@ heading.place(x=130,y=20)
 
 #main
 frame = Frame(root,width=400,height=50,bg="white")
-frame.place(x=0,y=180)
+frame.place(x=0,y=150)
 
 task = StringVar()
 task_entry = Entry(frame,width=18,font="arial 20",bd=0)
 task_entry.place(x=10,y=7)
 task_entry.focus()
 
-button = Button(frame, text="ADD", font="arial 20 bold", width=6,bg="#5a95ff", fg="#fff",bd=0,command=addTask)
-button.place(x=300, y=0)
+button = Button(frame, text="ADD", font="arial 20 bold", width=6,bg="#5a95ff", fg="red", bd=0, command=addTask)
+button.place(x=300, y=7)
 
 #listbox
-frame1 = Frame(root,bd=3,width=700,height=280,bg="#32405b")
-frame1.pack(pady=(160,0))
+frame1 = Frame(root, bd=3, width=400, height=280, bg="#32405b")
+frame1.place(x=0, y=200)
 
-listbox = Listbox(frame1,font=('arial',12),width=40,height=16,bg='#32405b', fg="white", cursor="hand2",selectbackground="#5a95ff")
-listbox.pack(side=LEFT , fill=BOTH, padx=2)
+listbox = Listbox(frame1,font=('arial',18),width=34,height=17,bg='#32405b', fg="white", cursor="hand2",selectbackground="#5a95ff")
+listbox.pack(side=LEFT, padx=2)
 scrollbar= Scrollbar(frame1)
 scrollbar.pack(side= RIGHT ,fill= BOTH)
 
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
-
-
 #delete
 Delete_icon = PhotoImage(file="image/delete.png")
-Button(root,image=Delete_icon,bd=0,command=deleteTask).pack(side=BOTTOM,pady=13)
+Button(root,image=Delete_icon,bd=0,command=deleteTask).pack(side=BOTTOM,pady=11)
 
 root.mainloop()
